9bb306fa3dc916f93879b6be2c09919932fb006b
Chawanakorn-Wongtaso <patchawanakorn@gmail.com>
2023-02-23 02:08:14 +0700

    navbar & logo.svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      colors: {
+      'sunglow': {
+        100: '#ffebc4',
+        200: '#fedb92',
+        300: '#fecf71',
+        400: '#fec44f',
+        500: '#feb92f',
+        600: '#d59b27',
+        700: '#ad7e1f',
+        800: '#846018',
+        900: '#5b4311',
+      }
+    },},
   },
   plugins: [],
 }
