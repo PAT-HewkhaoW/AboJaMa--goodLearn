48464b5d1a31761646000bffb0acb4c7bf20af37
Chawanakorn-Wongtaso <patchawanakorn@gmail.com>
2023-02-26 14:22:27 +0700

    link tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    
     extend: {
       colors: {
-      'sunglow': {
-        100: '#ffebc4',
-        200: '#fedb92',
-        300: '#fecf71',
-        400: '#fec44f',
-        500: '#feb92f',
-        600: '#d59b27',
-        700: '#ad7e1f',
-        800: '#846018',
-        900: '#5b4311',
-      },
-      'whisper':{
-        200: '#F9F9FB',
-        500: '#F4F3F8',
-        900: '#585759',
+        sunglow: {
+          100: "#ffebc4",
+          200: "#fedb92",
+          300: "#fecf71",
+          400: "#fec44f",
+          500: "#feb92f",
+          600: "#d59b27",
+          700: "#ad7e1f",
+          800: "#846018",
+          900: "#5b4311",
+        },
+        whisper: {
+          200: "#F9F9FB",
+          500: "#F4F3F8",
+          900: "#585759",
+        },
       },
 
+      fontFamily: {
+        sarabun: ['"Sarabun"', "sans-serif"],
+      },
     },
-    
-    fontFamily: {
-      'sarabun': ['"Sarabun"', 'sans-serif'],
-    }
-  
-  
-  },
   },
   plugins: [],
-}
+};
